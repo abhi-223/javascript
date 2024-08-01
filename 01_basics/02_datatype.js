e04d690c0a57434d458faabac539c088519371e1
abhi-223 <rawatabhishek223@gmail.com>
2024-08-01 08:18:58 +0000

    datatype

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,26 @@
+ "use strict"; // treat all js code as newer version
+ // alert (3+3) // we are using nodejs not browser
+
+ console.log(3+3) // code readability should be high
+ console.log("ram")
+
+ let name = "ram"
+ let age = 18
+ let isloggedIn = false
+ let state;
+
+
+ // number => 2 to power  53
+ // bigint
+ // string => ""
+ // boolean => true/false
+ // null => standalone value
+ // undefined =>
+ // symbol => unique
+
+ // object
+
+ console.log(typeof "ram");
+ console.log(typeof age);
+ console.log(typeof null);// object
+ console.log(typeof state);// undefined
