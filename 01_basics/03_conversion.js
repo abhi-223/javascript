2efb7624bcf4283d57eb84d6c8ec6d91ab3cf47c
abhi-223 <rawatabhishek223@gmail.com>
2024-08-01 08:42:35 +0000

    conversion

---
+++ 01_basics/03_conversion.js
@@ -0,0 +1,23 @@
+let score = "44"
+
+console.log(typeof score)
+
+let ValueInNumber = Number(score) // CONVERT ALL OTHER FORMAT IN NUMBER
+console.log(typeof ValueInNumber);
+
+let isloggedIn = 1
+
+let BooleanisLoggedIn = Boolean(isloggedIn)
+console.log(BooleanisLoggedIn);
+
+// "33"=> 33
+// "3abc"=> NaN
+// "true"=>1
+// Null=>0
+// ""=>false
+// "ram"=> true
+
+let somenumber= 33
+
+let Stringsomenumber = String(somenumber)
+console.log(Stringsomenumber);
