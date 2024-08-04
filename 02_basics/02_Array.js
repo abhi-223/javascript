146a8aa5db79f3b1a62e3bb89e6a628d2188a143
abhi-223 <rawatabhishek223@gmail.com>
2024-08-04 07:34:07 +0000

    Array

---
+++ 02_basics/02_Array.js
@@ -0,0 +1,11 @@
+const marvel_heroes = ["thor", "ironman", "spiderman"]
+
+const dc = ["superman", "flash","batman"]
+
+//marvel_heroes.push(dc)
+//console.log(marvel_heroes);
+
+const newhero = marvel_heroes.concat(dc)
+console.log(newhero);
+
+
