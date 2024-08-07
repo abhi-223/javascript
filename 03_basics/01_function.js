474ae2d1701f69b550d6d67cbc96ed3d38422278
abhi-223 <rawatabhishek223@gmail.com>
2024-08-07 06:13:44 +0000

    function

---
+++ 03_basics/01_function.js
@@ -0,0 +1,31 @@
+//console.log("h");
+//console.log("i");
+//console.log("t");
+
+function saymyname(){
+    console.log("h");
+    console.log("i");
+    console.log("t");
+}
+
+
+//saymyname()
+
+//function addtwono(num1, num2){
+//    console.log(num1 + num2);
+     
+//}
+
+function addtwono(num1, num2){
+   // let result = num1 + num2
+    //return result
+    return num1 + num2 
+}
+
+const result = addtwono(3,5)
+//console.log(result);
+
+function loginusermsg(username){
+    return `${username} just logged in `
+}
+console.log(loginusermsg("ram"))
