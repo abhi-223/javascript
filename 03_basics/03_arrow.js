fd1f3f53dde8d0a150005a2373be7bd5d8bf11db
abhi-223 <rawatabhishek223@gmail.com>
2024-08-09 14:38:35 +0000

    arrow

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -4,6 +4,50 @@ const user = {
 
     welcomemsg: function() {
         console.log(`${this.username}, welcome to website`);
+        console.log(this);
+        
         
     }
 }
+/*
+user.welcomemsg()
+user.username = "sam"
+user.welcomemsg()
+
+console.log(this);
+*/
+
+//function chai(){
+//    let username = "ram"
+//    console.log(this.username)
+    
+//}
+//chai()
+
+
+/*
+const chai = function(){
+    let username = "ram"
+    console.log(this.username);
+    
+}
+*/
+
+const chai = () => {
+    let username = "ram"
+    console.log(this);
+    
+}
+//chai()
+
+
+//const addtwo = (num1, num2) => {
+//    return num1 + num2
+//}
+
+
+//const addtwo = (num1, num2) =>  (num1 + num2)
+
+const addtwo = (num1, num2) =>  ({username: "Ram"})
+
+console.log(addtwo(3,5));
